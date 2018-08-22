b16465b7dfe968a2ba0a4e210f2b8bc896f8e9d0
admin <admin@iMac3-35.local>
2018-08-22 11:15:31 +0200

    transfert main page

---
+++ resources/views/home.blade.php
@@ -0,0 +1,73 @@
+@extends('main')
+
+@section('content')
+<div class="banner">
+    <h1>PIOSH</h1>
+    <b>Participer Inclure Oser par les Sciences Humaines</b>
+    <br>
+    <c>Cabinet de recherche-action</c>
+    <p>
+        <br><a href="#contact">Pour en savoir plus, contactez-nous !</a>
+    </p>
+</div>
+<div class="container">
+<div class="intro">Nous pensons que chacun.e est source et ressource dans la construction de son quotidien et peut ainsi en être acteur.trice. PIOSH est le support pour intégrer la participation à différentes échelles tout en accompagnant les professionnel.le.s (collectivités, associations, entreprises) à l’inclure dans leur pratique.</div>
+
+<div class="row">
+    <div class=" col-sm-12 col-md-6 col-lg-4">
+        <div class="card">
+            <img class="img-fluid" src="https://fakeimg.pl/1200x1000/ebebeb/?text=no-image&font=lobster" alt="picture description">
+
+            <br>
+            <h2 class="text-center">Nos thématiques</h2>
+            <p>Le début, <strong>le tout</strong> début</p>
+        </div>
+    </div>
+
+
+    <div class=" col-sm-12 col-md-6 col-lg-4">
+        <div class="card">
+            <img class="img-fluid"  src="https://fakeimg.pl/1200x1000/ebebeb/?text=no-image&font=lobster" alt="picture description">
+
+            <br>
+            <h2 class="text-center">Nos actions</h2>
+            <ul>
+                <li>Projets d'accompagnement</li>
+                <li>Formations</li>
+                <li>Ateliers de sensibilisation</li>
+            </ul>
+        </div>
+    </div>
+
+
+<div class=" col-sm-12 col-md-6 col-lg-4">
+    <div class="card">
+        <img class="img-fluid"  src="https://fakeimg.pl/1200x1000/ebebeb/?text=no-image&font=lobster" alt="picture description">
+
+        <br>
+        <h2 class="text-center">Nos méthodes</h2>
+        <ul>
+            <li>Les classiques (...)</li>
+            <li>Les populaires (...)</li>
+            <li>Les surprenants (...)</li>
+        </ul>
+    </div>
+</div>
+
+
+</div>
+
+<div class="row contact valign-middle" id="contact">
+    <div class="col-sm-12 col-md-6 col-lg-6">
+        <h2>PIOSH</h2>
+        <p>99, rue des Dunes<br>
+            35400 SAINT-MALO</p>
+    </div>
+    <div class="col-sm-12 col-md-6 col-lg-6">
+        <img class="img-fluid" src="https://fakeimg.pl/1200x1000/ebebeb/?text=no-image&font=lobster" alt="picture description">
+
+    </div>
+</div>
+</div>
+</div>
+@endsection
---
+++ resources/views/main.blade.php
@@ -0,0 +1,49 @@
+<html>
+  <head>
+    <meta charset="utf-8">
+    <title>PIOSH</title>
+    {{ HTML::style('/css/main.css') }}
+    <link type="text/css" rel="stylesheet" href="http://fonts.googleapis.com/css?family=Amatic+SC" /><link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Helvetica" />
+    <!-- FONT AWESOME -->
+<link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.2.0/css/all.css" integrity="sha384-hWVjflwFxL6sNzntih27bfxkr27PmbbK/iSvJ+a4+0owXq79v+lsFkW54bOGbiDQ" crossorigin="anonymous">    <!-- BOOTSTRAP -->
+    <script
+            src="https://code.jquery.com/jquery-3.3.1.js"
+            integrity="sha256-2Kok7MbOyxpgUVvAk/HJ2jigOSYS2auK4Pfzbm7uH60="
+            crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
+  </head>
+  <body>
+      @yield('content')
+    <footer>
+        <div class="container">
+
+            <ul>
+                <li>
+                    <a href="#"><i class="fab fa-facebook-f"></i>&nbsp;</a>
+                </li>
+                <li>
+                    <a href="#"><i class="fab fa-twitter"></i>&nbsp;</a>
+                </li>
+            </ul>
+        </div>
+
+    </footer>
+    {{ HTML::script('/js/main.js') }}
+  </body>
+</html>
+<script>
+    $('a[href*="#"]:not([href="#"])').not('a.modal-trigger, a.modal-close').click(function () {
+        if (location.pathname.replace(/^\//, '') === this.pathname.replace(/^\//, '') && location.hostname === this.hostname) {
+            var target = $(this.hash);
+            target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
+            if (target.length) {
+                $('html, body').animate({
+                    scrollTop: target.offset().top
+                }, 700);
+                return false;
+            }
+        }
+    });
+</script>
