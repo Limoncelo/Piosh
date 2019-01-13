23115c3a65b9acee240d9e68230580f4b9ab15b0
Lina <lina.maret@gmail.com>
2019-01-13 21:19:23 +0100

    photos liste actus

--- resources/views/articles/articles.blade.php
+++ resources/views/articles/articles.blade.php
@@ -7,7 +7,9 @@
 @foreach ($articles as $article)
     <div class="col-12 col-sm-6 col-lg-4">
       <div class="card article valign-middle my-3 text-center" id="art{{ $article->id }}">
-        <img class="card-img-top" src="{{ $article->photo_1 }}" alt="{{ $article->title }}">
+        @if(!empty($article->photo_1))
+          <img class="card-img-top" src="{{ $article->photo_1 }}" alt="{{ $article->title }}">
+        @endif
         <div class="card-body">
           <h5 class="card-title">{{ $article->title }}</h5>
           <p class="card-text">{!!  strlen($article->desc) > 100 ? substr($article->desc, 0, 100) . '...' : $article->desc !!}</p>
